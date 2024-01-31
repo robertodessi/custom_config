0d8dd7cd6720fe9b861932a1502583ef42285fad
robertodessi <roberto.dessi11@gmail.com>
2024-01-31 18:27:52 +0000

    upd

---
+++ upd_and_push.sh
@@ -0,0 +1,10 @@
+cp ~/.profile bash/profile
+cp ~/.bashrc bash/bashrc
+
+cp ~/.screenrc screenrc/screenrc
+cp ~/.screen_layout screenrc/screen_layout
+
+cp ~/.vimrc vimrc/vimrc
+
+git commit -am "upd"
+git push
